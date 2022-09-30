ce885ebd7e18107365482cbbf84dc6b203a7de1e
Hari  Roopan <harihara.r@w3dev.email>
2022-09-30 11:17:00 +0530

    Basic ML model completed

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, UploadFile
 from pdf2image import convert_from_path
 import cv2
 import shutil
 import pytesseract
+import keras_ocr
+from fastapi.middleware.cors import CORSMiddleware
+
 pytesseract.pytesseract.tesseract_cmd=r"C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe"
 app = FastAPI()
-
-
+origins = ["*","http://10.1.60.151:3000"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.post("/files/")
 async def create_file(file: UploadFile):
     async def writePdf():
@@ -17,14 +26,14 @@ async def create_file(file: UploadFile):
     async def imgToPdf(): 
         pages = convert_from_path(pdf_path="form.pdf",poppler_path="C:\\Program Files\\poppler-0.68.0\\bin")
         for i in range(len(pages)):
-            pages[i].save('img 1.jpg', 'JPEG')
+            pages[i].save('img 1.jpeg', 'JPEG')
     await imgToPdf()
     # Reading and resizing image
     try:
-        image1 = cv2.imread("D:\Joseph Hackathom\ML Model\\img 1.jpg")
+        image1 = cv2.imread("D:\Joseph Hackathom\ML Model\\img 1.jpeg")
         image1_rs = cv2.resize(image1, (800, 800)) 
-        cv2.imwrite("D:\Joseph Hackathom\ML Model\\img rsed.jpg",image1_rs)
-        img = cv2.imread("img rsed.jpg")
+        cv2.imwrite("D:\Joseph Hackathom\ML Model\\img rsed.jpeg",image1_rs)
+        img = cv2.imread("img rsed.jpeg")
         def getFields(img):
             crop_name = img[110:150, 227:600]  
             crop_age = img[150:190,227:375]
@@ -40,16 +49,18 @@ async def create_file(file: UploadFile):
             cv2.imwrite("D:\Joseph Hackathom\ML Model\\state.jpeg",crop_state)
             cv2.imwrite("D:\Joseph Hackathom\ML Model\\phone.jpeg",crop_phone)
             cv2.imwrite("D:\Joseph Hackathom\ML Model\\email.jpeg",crop_email)
-        getFields(img)    
+        getFields(img)   
+        url=["D:\Joseph Hackathom\ML Model\\name.jpeg","D:\Joseph Hackathom\ML Model\\age.jpeg"]
+        images = [keras_ocr.tools.read(i) for i in url]
+        preds=[]
+        for i in images:
+            preds.append(pytesseract.image_to_string(i))
     except Exception as e:
         print(str(e))   
-         
-    
-    
     cv2.waitKey(0) 
     cv2.destroyAllWindows()
     
-    return {"file_name": file.filename}  
+    return {"name": preds[0],"age":preds[1]}  
 
 @app.get("/")
 def welcome():
